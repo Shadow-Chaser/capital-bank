a979e20221334c7eb5bb3b8c4d9d0cb28976f79b
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-01-26 14:01:37 +0600

    Deposit Button event handler

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+// Login Button event handler
 const loginBtn = document.getElementById("login");
 loginBtn.addEventListener("click", function(){
-    console.log("Clicked");
+
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+})
+
+// Deposit Button event handler
+const depositBtn = document.getElementById("add-deposit");
+depositBtn.addEventListener("click", function(){
+    const depositAmount = document.getElementById("deposit-amount").value;
+    const depositNumber = parseFloat(depositAmount);
+
+    const currentDeposit = document.getElementById("current-deposit").innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositNumber + currentDepositNumber;
+    
+    document.getElementById("current-deposit").innerText = totalDeposit;
+    document.getElementById("deposit-amount").value = "";
+
 })
